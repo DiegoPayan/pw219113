ecd46ee1e55c6363f9a9b0118689b2e2765425ce
Centro de Computo Dispositivos Moviles <ccdmadm@iMac-de-Centro-9.local>
2019-05-07 13:55:18 -0600

    Primer practica VUEJS

---
+++ vue/js/primervue.js
@@ -0,0 +1,15 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        titulo: "Hola Mundo",
+        frutas: ['1,2,3,4,5'],
+        frutas2: [
+            {nombre:'0',cantidad:0},
+            {nombre:'1',cantidad:11},
+            {nombre:'2',cantidad:22},
+            {nombre:'3',cantidad:33},
+            {nombre:'4',cantidad:4},
+            {nombre:'5',cantidad:5}
+        ]
+    }
+});
