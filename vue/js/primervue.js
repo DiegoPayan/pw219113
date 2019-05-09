ddc57ca726cd3d331df2065be353d0c1165dea02
Centro de Computo Dispositivos Moviles <ccdmadm@iMac-de-Centro-9.local>
2019-05-09 13:56:37 -0600

    Se agrega funcionalidad a los componentes

--- vue/js/primervue.js
+++ vue/js/primervue.js
@@ -10,6 +10,33 @@ const app = new Vue({
             {nombre:'3',cantidad:33},
             {nombre:'4',cantidad:4},
             {nombre:'5',cantidad:5}
-        ]
+        ],
+        nuevaFruta: '',
+        total: 0
+    },
+    methods: {
+        agregarFruta() {
+            if(this.nuevaFruta) {
+                this.frutas.push(this.nuevaFruta);
+                this.frutas2.push({
+                    nombre: this.nuevaFruta,
+                    cantidad: 0
+                });
+                this.nuevaFruta = '';
+            }
+        },
+        eliminaFruta() {
+            this.frutas.pop();
+            this.frutas2.pop();
+        }
+    },
+    computed: {
+        sumarFrutas() {
+            this.total = 0;
+            for(fruta of this.frutas2) {
+                this.total += fruta.cantidad;
+            }
+            return this.total;
+        }
     }
 });
